0ae1093f7bf890b439ed5ec72403a1e8b07f2b43
drkphnx <dark.phnx12@gmail.com>
2024-01-08 13:54:42 +0100

    Alioth:bringup matrix

--- lineage_alioth.mk
+++ lineage_alioth.mk
@@ -9,23 +9,26 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common lineage stuff.
-$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := bliss_alioth
+PRODUCT_NAME := lineage_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO F3
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 2400
-TARGET_SCREEN_WIDTH := 1080
-TARGET_BOOT_ANIMATION_RES := 1080
+MATRIXX_BUILD_TYPE := Official
+MATRIXX_MAINTAINER := Dark_Phoenix
+MATRIXX_CHIPSET := SD-870
+MATRIXX_BATTERY := 4520mAh
+MATRIXX_DISPLAY := 1080x2400
+WITH_GMS := true
+TARGET_BOOT_ANIMATION_RES := 1440
 
-BLISS_BUILDTYPE := OFFICIAL
 TARGET_SUPPORTS_QUICK_TAP := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
